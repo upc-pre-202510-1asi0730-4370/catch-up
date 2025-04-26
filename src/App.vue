29f66c261c707ec40acfb3948ec8d9d32db65a51
Alberto Sanchez S. <albertosanchez05@gmail.com>
2025-04-25 21:17:42 -0500

    feat: add source-list and source-item

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script>
 import LanguageSwitcher from "@/public/language-switcher.component.vue";
 import FooterContent from "@/public/footer-content.component.vue";
+import SourceList from "@/news/components/source-list.component.vue";
+import {NewsApiService} from "@/news/services/news-api.service.js";
+import {SourceAssembler} from "@/news/services/source.assembler.js";
 
 export default {
   name: "App",
-  components: {FooterContent, LanguageSwitcher},
+  components: {SourceList, FooterContent, LanguageSwitcher},
+  data() {
+    return {
+      drawerVisible: true,
+      articles: [],
+      sources: [],
+      errors: [],
+      newsApi: new NewsApiService()
+    }
+  },
+  created() {
+    this.getSources();
+  },
   methods: {
+    getSources() {
+      this.newsApi.getSources()
+          .then(response => {
+            console.log(response);
+            this.sources = SourceAssembler.toEntitiesFromResponse(response);
+          })
+          .catch(e => {
+            this.errors.push(e);
+            this.sources = [];
+          });
+    },
     toggleSidebar() {
-
+      this.drawerVisible = !this.drawerVisible;
     }
   }
 }
@@ -20,6 +46,7 @@ export default {
       <pv-menubar>
         <template #start>
           <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleSidebar" />
+          <source-list v-model:visible="drawerVisible" v-model:sources="sources" />
         </template>
         <template #end>
           <language-switcher></language-switcher>
---
+++ src/news/components/source-list.component.vue
@@ -0,0 +1,23 @@
+<script>
+import SourceItem from "@/news/components/source-item.component.vue";
+import {Source} from "@/news/model/source.entity.js";
+
+export default {
+  name: "source-list",
+  components: {SourceItem},
+  props: {
+    sources: Array[Source],
+    visible: Boolean,
+  }
+}
+</script>
+
+<template>
+  <pv-drawer v-bind:visible="visible">
+    <source-item v-for="source in sources" :source="source" :key="source.id" />
+  </pv-drawer>
+</template>
+
+<style scoped>
+
+</style>
